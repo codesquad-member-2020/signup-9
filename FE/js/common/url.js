4a9cd6b061bd646f38c4a503d181eb3489e35944
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-03-25 22:03:44 +0900

    [FEAT] url 경로 파일 추가
    url 경로 파일 추가 >
    * 서버로 api 를 통한 요청을 하기 위해 사용되는 url 추가.

---
+++ FE/js/common/url.js
@@ -0,0 +1,17 @@
+const MOCKUP_URL = Object.freeze({
+    ID: "http://37b0ab9f-726b-4389-9041-add27b33e400.mock.pstmn.io/user-id-vaildation",
+    EMAIL: "http://37b0ab9f-726b-4389-9041-add27b33e400.mock.pstmn.io/email-vaildation",
+    PHONE: "http://37b0ab9f-726b-4389-9041-add27b33e400.mock.pstmn.io/phone-number-vaildation",
+    JOIN: "http://37b0ab9f-726b-4389-9041-add27b33e400.mock.pstmn.io/user-registration",
+    LOGIN: "http://37b0ab9f-726b-4389-9041-add27b33e400.mock.pstmn.io/login"
+});
+
+const SERVICE_URL = Object.freeze({
+    ID: "https://cors-anywhere.herokuapp.com/hyunjun.herokuapp.com/user-id-vaildation",
+    EMAIL: "https://cors-anywhere.herokuapp.com/hyunjun.herokuapp.com/email-vaildation",
+    PHONE: "https://cors-anywhere.herokuapp.com/hyunjun.herokuapp.com/phone-number-vaildation",
+    JOIN: "",
+    LOGIN: ""
+});
+
+export default {MOCKUP_URL, SERVICE_URL};
