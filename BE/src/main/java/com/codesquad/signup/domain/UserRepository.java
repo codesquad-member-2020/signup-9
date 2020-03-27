9c559cde238e3c92843e3f95103af42fbac3443d
guswns1659 <guswns1659@gmail.com>
2020-03-27 12:59:47 +0900

    [FEAT] 로그인 기능 구현

--- BE/src/main/java/com/codesquad/signup/domain/UserRepository.java
+++ BE/src/main/java/com/codesquad/signup/domain/UserRepository.java
@@ -9,12 +9,15 @@ import java.util.Optional;
 public interface UserRepository extends CrudRepository<User, Long> {
 //    @Query("select * from User u where u.user_Id = :userId")
 //    Optional<User> findByUserId(@Param("userId") String userId);
-    @Query("select USER_ID from User u where u.user_Id = :userId")
-    Optional<String> findByUserId(@Param("userId") String userId);
+@Query("select USER_ID from User u where u.user_Id = :userId")
+Optional<String> findByUserId(@Param("userId") String userId);
 
     @Query("select EMAIL from User u where u.email = :email")
     Optional<String> findByEmail(@Param("email") String email);
 
     @Query("select PHONE_NUMBER from User u where u.PHONE_NUMBER = :phoneNumber")
     Optional<String> findByPhoneNumber(@Param("phoneNumber") String phoneNumber);
+
+    @Query("select PASSWORD from User u where u.PASSWORD = :password")
+    Optional<String> findByPassword(@Param("password") String password);
 }
